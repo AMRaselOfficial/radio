89cfaea8e869315b4952f067b629f80987c50da8
Abdul Momin Rasel <72349866+AMRaselOfficial@users.noreply.github.com>
2025-08-02 09:48:43 +0600

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/ping', (req, res) => {
 const ffmpeg = spawn('ffmpeg', [
   '-stream_loop', '-1', // 🔁 Loop forever
   '-re',
-  '-i', 'https://www.dropbox.com/scl/fi/lovvfj4uie9zjdqdxiyfd/YouTubers-Playlist-2022.mp3?rlkey=it8jk7qkiqm6lz46srehzc84l&st=bttgjxt4&dl=1',
+  '-i', 'https://www.dropbox.com/scl/fi/z1bv06l2ao11q9ksmyl8w/You-re-listening-to-AM-Rasel-s-Official-Radio.mp3?rlkey=6jl6m2qapzvgnqv38ihl6wthm&st=qvtppz23&dl=1',
   '-f', 'mp3',
   'pipe:1'
 ]);
