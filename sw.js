854bf54d443c711529ec01de49447fa6c88f4f24
Abdul Momin Rasel <72349866+AMRaselOfficial@users.noreply.github.com>
2025-09-14 00:59:47 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'rasel-radio-cache-v1';
+const urlsToCache = [
+  '/radio/index.html',
+  '/radio/',         // root of radio
+  '/radio/style.css',// if you have CSS
+  '/radio/script.js',// if you have JS
+  // you may want to cache your audio file or parts of it (if small)
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return from cache if available, else fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
